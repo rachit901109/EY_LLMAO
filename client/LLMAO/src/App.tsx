0509acb2811c6751e74f50a5eb71fec510a19327
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2023-11-13 00:21:54 +0530

    Home page added

--- client/LLMAO/src/App.tsx
+++ client/LLMAO/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './views/Login'; // adjust the path to your Login component as necessary
 import Signup from './views/Signup'
+import Home from './views/Home';
+
 function App() {
   return (
     <Router>
@@ -8,6 +10,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         {/* // add more routes here as necessary */}
       </Routes>
     </Router>
