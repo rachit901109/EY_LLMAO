154fdcddbd28af5a2d8fc38936f36d41213af01c
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2023-11-14 14:32:14 +0530

    trending page updated

--- client/LLMAO/src/views/Trending.tsx
+++ client/LLMAO/src/views/Trending.tsx
@@ -8,15 +8,13 @@ function Trending() {
     "Title 2": "Content 2",
     "Title 3": "Content 3",
     "Title 4": "Content 4",
-    "Title 5": "Content 5",
-    "Title 6": "Content 6",
     // Add more titles and content as needed
   };
 
   return (
     <>
       <Navbar />
-      <Grid templateColumns="repeat(3, 1fr)" gap={4} mt={2} mx={4}>
+      <Grid templateColumns="repeat(4, 1fr)" gap={4} mt={2} mx={4}>
         {Object.entries(data).map(([title, content]) => (
           <MyCard key={title} title={title} content={content} />
         ))}
